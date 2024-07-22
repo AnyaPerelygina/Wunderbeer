b0731382de4852f07eea45080004a9a15647e74c
Anna <mynameisannanas@gmail.com>
2024-07-22 23:17:18 +0200

    fix: правит пути картинок (тест)

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  basePath: isProd ? '/public' : '',
+  basePath: isProd ? '/wunderbeer/public' : '',
   distDir: 'out',
   images: {
     unoptimized: true,
