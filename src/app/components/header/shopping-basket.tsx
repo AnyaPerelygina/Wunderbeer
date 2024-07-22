3c369a1cee31e31b7062dcb2e1411802fa878ce2
Anna <mynameisannanas@gmail.com>
2024-07-22 23:25:55 +0200

    fix: правит путь у картинки корзины

--- src/app/components/header/shopping-basket.tsx
+++ src/app/components/header/shopping-basket.tsx
@@ -1,16 +1,15 @@
 'use client';
 
-// import { basePath } from '@/const';
+import { basePath } from '@/const';
 import Image from 'next/image';
 import Link from 'next/link';
-import basket from '../../../../public/svg/shopping-basket.svg';
 
 export default function ShoppingBasket() {
   return (
     <div className='shopping-basket'>
       <Link href={'#'}>
         <Image
-          src={basket}
+          src={`${basePath}/svg/shopping-basket.svg`}
           width={28}
           height={30}
           alt={'Корзина с товарами.'} />
