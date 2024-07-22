4538aa255f4575810ff921e7c501c52053a36777
Anna <mynameisannanas@gmail.com>
2024-07-22 23:03:37 +0200

    fix: правит пути картинок (тест)

--- src/app/components/header/shopping-basket.tsx
+++ src/app/components/header/shopping-basket.tsx
@@ -3,13 +3,14 @@
 // import { basePath } from '@/const';
 import Image from 'next/image';
 import Link from 'next/link';
+import basket from '../../../../public/svg/shopping-basket.svg';
 
 export default function ShoppingBasket() {
   return (
     <div className='shopping-basket'>
       <Link href={'#'}>
         <Image
-          src={'../../../../svg/shopping-basket.svg'}
+          src={basket}
           width={28}
           height={30}
           alt={'Корзина с товарами.'} />
