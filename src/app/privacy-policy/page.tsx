82b9323aae3360206d1a4967aa90de06b3f19167
Anna <mynameisannanas@gmail.com>
2025-01-13 21:47:48 +0100

    feat: добавляет политику конфиденциальности

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../ui/banner/banner';
+import PrivacyPolicyDoc from '@/app/components/privacy-policy-doc/privacy-policy-doc';
 
 export default function PrivacyPolicy() {
   return (
     <React.Fragment>
       <Banner image={'banner-1.webp'} title={'Политика конфиденциальности'}/>
+      <PrivacyPolicyDoc />
     </React.Fragment>
   )
 }
