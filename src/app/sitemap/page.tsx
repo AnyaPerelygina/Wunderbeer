7202b3554feb34835afff614a4d0db2aed2de51f
Anna <mynameisannanas@gmail.com>
2025-01-24 15:13:59 +0100

    fix: корректирует sitemap разметку и стили

--- src/app/sitemap/page.tsx
+++ src/app/sitemap/page.tsx
@@ -7,21 +7,40 @@ import styles from './sitemap.module.scss';
 export default function Sitemap() {
   return (
     <div className={styles.sitemap}>
-      <Image
-        src={`${basePath}/logo/logo.webp`}
-        width={94}
-        height={111}
-        alt="Логотип Wunderbeer."
-      />
-      <br /><br />
-      <Link href={'/home'}>1. Главная;</Link>
-      <Link href={'/error'}>2. Ошибка загрузки страницы;</Link>
-      <Link href={'/privacy-policy'}>3. Политика конфиденциальности;</Link>
-      <Link href={'/contacts'}>4. Контакты;</Link>
-      <Link href={'/about-us'}>5. О компании;</Link>
-      <Link href={'/equipment-of-shops'}>6. Комплектация магазинов;</Link>
-      <Link href={'/distribution'}>7. Дистрибуция;</Link>
-      <Link href={'/catalog'}>8. Каталог;</Link>
+      <div className={styles.logo}>
+        <Image
+          src={`${basePath}/logo/logo.webp`}
+          width={94}
+          height={111}
+          alt="Логотип Wunderbeer."
+        />
+      </div>
+      <ul className={styles.list}>
+        <li className={styles.item}>
+          <Link href={'/home'}>Главная;</Link>
+        </li>
+        <li className={styles.item}>
+          <Link href={'/catalog'}>Каталог;</Link>
+        </li>
+        <li className={styles.item}>
+          <Link href={'/distribution'}>Дистрибуция;</Link>
+        </li>
+        <li className={styles.item}>
+          <Link href={'/equipment-of-shops'}>Комплектация магазинов;</Link>
+        </li>
+        <li className={styles.item}>
+          <Link href={'/about-us'}>О компании;</Link>
+        </li>
+        <li className={styles.item}>
+          <Link href={'/contacts'}>Контакты;</Link>
+        </li>
+        <li className={styles.item}>
+          <Link href={'/privacy-policy'}>Политика конфиденциальности;</Link>
+        </li>
+        <li className={styles.item}>
+          <Link href={'/error'}>Ошибка загрузки страницы;</Link>
+        </li>
+      </ul>
     </div>
   )
 }
