81670e7d1da870ea130726944288dfc7a5c0c5c8
Anna <mynameisannanas@gmail.com>
2025-03-23 21:58:22 +0100

    feat: добавляет стили корзине mobile

--- src/app/components/shopping-card-block/shopping-cart-block.tsx
+++ src/app/components/shopping-card-block/shopping-cart-block.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState, useEffect } from 'react';
 import { useCart } from "@/context/cart-context";
 import { Container } from '../../../ui/container/container';
 import LinkNew from '@/ui/link/link';
@@ -12,17 +13,28 @@ import Trash from '@/assets/trash.svg';
 
 export default function ShoppingCartBlock() {
   const { items, removeItemCompletely, updateItemQuantity } = useCart();
+  const [isMobileScreen, setIsMobileScreen] = useState(false);
+
+  useEffect(() => {
+      const handleResize = () => {
+        setIsMobileScreen(window.innerWidth < 768);
+      };
+
+      handleResize();
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
   const handleDelete = (productKey: string) => {
-    removeItemCompletely(productKey); // Удаляем товар полностью из корзины
+    removeItemCompletely(productKey);
   };
 
   const handleIncrement = (productKey: string) => {
-    updateItemQuantity(productKey, 1); // Увеличиваем количество товара на 1
+    updateItemQuantity(productKey, 1);
   };
 
   const handleDecrement = (productKey: string) => {
-    updateItemQuantity(productKey, -1); // Уменьшаем количество товара на 1
+    updateItemQuantity(productKey, -1);
   };
 
   return (
@@ -37,61 +49,111 @@ export default function ShoppingCartBlock() {
           </div>
         ) : (
           <div className={styles.fullWrapper}>
-            <table className={styles.table}>
-              <thead className={styles.tabWrapper}>
-                <tr className={styles.tabListTitle}>
-                  <th className={styles.visuallyHidden}>Изображение</th>
-                  <th className={styles.tabTitle}>Наименование товара</th>
-                  <th className={styles.tabTitle}>Кол-во</th>
-                  <th className={styles.tabTitle}>Стоимость</th>
-                  <th className={styles.visuallyHidden}>Расчет стоимости товара</th>
-                  <th className={styles.visuallyHidden}>Удаление товара</th>
-                </tr>
-              </thead>
-              <tbody className={styles.tabWrapper}>
-                {items.map((item) => (
-                  <tr key={item.productKey} className={styles.tabItem}>
-                    <td className={styles.productImg}>
-                      <Image
-                        src={`${basePath}/bottles/${item.image}.webp`}
-                        width={66}
-                        height={173}
-                        alt={'Изображение пивной бутылки.'}
-                      />
-                    </td>
-                    <td className={styles.productInfo}>
-                      <span>{item.name}</span>
-                      <span>{item.productType} {item.name}</span>
-                      <span>{item.description}</span>
-                      <span>{item.size}</span>
-                    </td>
-                    <td className={styles.productQuantity}>
-                      <div className={styles.productQuantityWrapper}>
-                        <button onClick={() => handleDecrement(item.productKey)}></button>
-                        <span className={styles.counterNumber}>{item.quantity}</span>
-                        <button onClick={() => handleIncrement(item.productKey)}></button>
+            {isMobileScreen ? (
+              <div className={styles.wrapper}>
+                <ul className={styles.list}>
+                  {items.map((item) => (
+                    <li key={item.productKey} className={styles.item}>
+                      <div className={styles.productImg}>
+                        <Image
+                          src={`${basePath}/bottles/${item.image}.webp`}
+                          width={66}
+                          height={173}
+                          alt={'Изображение пивной бутылки.'}
+                        />
                       </div>
-                    </td>
-                    <td className={styles.productPrice}>{item.price * item.quantity} ₽</td>
-                    <td className={styles.productСalculation}>{item.price} ₽ * {item.quantity}</td>
-                    <td className={styles.btnDeleteProduct}>
-                      <button onClick={() => handleDelete(item.productKey)}>
-                        <Icon Icon={Trash} width={16} height={16} />
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <div className={styles.result}>
-              <div className={styles.totalPrice}>
-                <span>Итого:</span>
-                <span>{items.reduce((total, item) => total + item.price * item.quantity, 0)} ₽</span>
+                      <div className={styles.productInfo}>
+                        <span>{item.name}</span>
+                        <span>{item.productType} {item.name}</span>
+                        <span>{item.description}</span>
+                        <span>{item.size} л</span>
+                      </div>
+                      <div className={styles.productQuantity}>
+                        <div className={styles.productQuantityWrapper}>
+                          <button onClick={() => handleDecrement(item.productKey)}></button>
+                          <span className={styles.counterNumber}>{item.quantity}</span>
+                          <button onClick={() => handleIncrement(item.productKey)}></button>
+                        </div>
+                      </div>
+                      <div className={styles.productPrice}>{item.price * item.quantity} ₽</div>
+                      <div className={styles.productСalculation}>{item.price} ₽ * {item.quantity}</div>
+                      <div className={styles.btnDeleteProduct}>
+                        <button onClick={() => handleDelete(item.productKey)}>
+                          <Icon Icon={Trash} width={16} height={16} />
+                        </button>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+                <div className={styles.result}>
+                  <div className={styles.totalPrice}>
+                    <span>Итого:</span>
+                    <span>{items.reduce((total, item) => total + item.price * item.quantity, 0)} ₽</span>
+                  </div>
+                  <div className={styles.btnOrder}>
+                    <LinkNew href={"#"}>Перейти к оформлению</LinkNew>
+                  </div>
+                </div>
               </div>
-              <div className={styles.btnOrder}>
-                <LinkNew href={"#"}>Оформить заказ</LinkNew>
+            ) : (
+            <div className={styles.wrapper}>
+              <table className={styles.table}>
+                <thead className={styles.tabWrapper}>
+                  <tr className={styles.tabListTitle}>
+                    <th className={styles.visuallyHidden}>Изображение</th>
+                    <th className={styles.tabTitle}>Наименование товара</th>
+                    <th className={styles.tabTitle}>Кол-во</th>
+                    <th className={styles.tabTitle}>Стоимость</th>
+                    <th className={styles.visuallyHidden}>Расчет стоимости товара</th>
+                    <th className={styles.visuallyHidden}>Удаление товара</th>
+                  </tr>
+                </thead>
+                <tbody className={styles.tabWrapper}>
+                  {items.map((item) => (
+                    <tr key={item.productKey} className={styles.tabItem}>
+                      <td className={styles.productImg}>
+                        <Image
+                          src={`${basePath}/bottles/${item.image}.webp`}
+                          width={66}
+                          height={173}
+                          alt={'Изображение пивной бутылки.'}
+                        />
+                      </td>
+                      <td className={styles.productInfo}>
+                        <span>{item.name}</span>
+                        <span>{item.productType} {item.name}</span>
+                        <span>{item.description}</span>
+                        <span>{item.size} л</span>
+                      </td>
+                      <td className={styles.productQuantity}>
+                        <div className={styles.productQuantityWrapper}>
+                          <button onClick={() => handleDecrement(item.productKey)}></button>
+                          <span className={styles.counterNumber}>{item.quantity}</span>
+                          <button onClick={() => handleIncrement(item.productKey)}></button>
+                        </div>
+                      </td>
+                      <td className={styles.productPrice}>{item.price * item.quantity} ₽</td>
+                      <td className={styles.productСalculation}>{item.price} ₽ * {item.quantity}</td>
+                      <td className={styles.btnDeleteProduct}>
+                        <button onClick={() => handleDelete(item.productKey)}>
+                          <Icon Icon={Trash} width={16} height={16} />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <div className={styles.result}>
+                <div className={styles.totalPrice}>
+                  <span>Итого:</span>
+                  <span>{items.reduce((total, item) => total + item.price * item.quantity, 0)} ₽</span>
+                </div>
+                <div className={styles.btnOrder}>
+                  <LinkNew href={"#"}>Перейти к оформлению</LinkNew>
+                </div>
               </div>
             </div>
+            )}
           </div>
         )}
       </Container>
