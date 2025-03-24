35238c49d7e6cccc9c88a71a0a684d594ddeb879
Anna <mynameisannanas@gmail.com>
2025-03-24 20:59:06 +0100

    feat: добавляет Выбранные товары в Оформление заказа

--- src/app/components/order-block/order-block.tsx
+++ src/app/components/order-block/order-block.tsx
@@ -1,12 +1,54 @@
+'use client'
+
+import Image from 'next/image';
+
+import { useCart } from "@/context/cart-context";
+import { basePath } from '@/const';
 import { Container } from '@/ui/container/container';
-import styles from './order-block.module.scss';
 import FormOrders from '../forms/form-orders/form-orders';
 
+import styles from './order-block.module.scss';
+
 export default function OrderBlock() {
+  const { items } = useCart();
+
   return (
     <section className={styles.root}>
       <Container className={styles.container}>
+        <h2 className={styles.title}>Ваши контакты</h2>
         <FormOrders />
+        <div className={styles.shoppingCart}>
+          <h3 className={styles.subTitle}>Выбранные товары</h3>
+          <ul className={styles.list}>
+            {items.map((item) => (
+              <li key={item.productKey} className={styles.item}>
+                <div className={styles.productImg}>
+                  <Image
+                    src={`${basePath}/bottles/${item.image}.webp`}
+                    width={66}
+                    height={173}
+                    alt={'Изображение пивной бутылки.'}
+                  />
+                </div>
+                <div className={styles.productInfo}>
+                  <span>{item.name}</span>
+                  <span>{item.productType} {item.name}</span>
+                  <span>{item.description}</span>
+                  <span>{item.size} л</span>
+                  <span className={styles.counterNumber}>{item.quantity} шт.</span>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className={styles.allInfo}>
+            <span>Всего товаров</span>
+            <span>{items.reduce((total, item) => total + item.quantity, 0)} шт.</span>
+          </div>
+          <div className={styles.total}>
+            <span>Итого:</span>
+            <span>{items.reduce((total, item) => total + item.price * item.quantity, 0)} ₽</span>
+          </div>
+        </div>
       </Container>
     </section>
   )
