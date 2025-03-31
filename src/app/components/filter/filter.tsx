847e9762d7bb4ff924ea2b7ee8b15d888f011f72
Anna <mynameisannanas@gmail.com>
2025-04-01 01:58:31 +0200

    fix: корректирует разметку фильтра

--- src/app/components/filter/filter.tsx
+++ src/app/components/filter/filter.tsx
@@ -10,7 +10,6 @@ import Icon from '@/ui/icon/icon';
 import WheatYellow from '@/assets/wheat-yellow.svg';
 
 export default function Filter({ applyFilters }: FilterProps) {
-  const [isMobileScreen, setIsMobileScreen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const filterRef = useRef<HTMLDivElement>(null);
 
@@ -26,38 +25,21 @@ export default function Filter({ applyFilters }: FilterProps) {
     };
   }, [filterRef]);
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobileScreen(window.innerWidth < 1024);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <div className={`${styles.root} ${isOpen ? styles['is-opened'] : ''}`}>
-      {!isMobileScreen && (
-        <div className={styles.wrapper}>
+      <div ref={filterRef} className={`${styles.wrapper} ${isOpen ? styles['is-opened'] : ''}`}>
+        <button className={`${styles.toggle} ${isOpen ? styles['is-opened'] : ''}`} onClick={() => setIsOpen(!isOpen)}>
+          <span className={styles.toggleOpened}>
+            <Icon path={WheatYellow} width={47} height={18} />
+              Фильтр
+            <Icon path={WheatYellow} width={47} height={18} />
+          </span>
+          <span className={styles.toggleClosed}>Закрыть</span>
+        </button>
+        <div className={`${styles.filterMenu} ${isOpen ? styles['is-opened'] : ''}`}>
           <FilterBlock applyFilters={applyFilters} />
         </div>
-      )}
-      {isMobileScreen && (
-        <div ref={filterRef} className={`${styles.wrapper} ${isOpen ? styles['is-opened'] : ''}`}>
-          <button className={`${styles.toggle} ${isOpen ? styles['is-opened'] : ''}`} onClick={() => setIsOpen(!isOpen)}>
-            <span className={styles.toggle__opened}>
-              <Icon path={WheatYellow} width={47} height={18} />
-                Фильтр
-              <Icon path={WheatYellow} width={47} height={18} />
-            </span>
-            <span className={styles.toggle__closed}>Закрыть</span>
-          </button>
-          <div className={`${styles.filterMenu} ${isOpen ? styles['is-opened'] : ''}`}>
-            <FilterBlock applyFilters={applyFilters} />
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   )
 }
