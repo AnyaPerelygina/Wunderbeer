dbe2bddbc529c5d7ad225cc6a8e1d12b7af314c2
Anna <mynameisannanas@gmail.com>
2025-04-01 02:10:25 +0200

    fix: вносит правки в категории фильтров

--- src/app/components/filter/filter-block/filter-block.tsx
+++ src/app/components/filter/filter-block/filter-block.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import React, { useState, useMemo, ChangeEvent } from 'react';
 
 import styles from './filter-block.module.scss';
 import { FilterBlockProps } from './filter-block.types';
@@ -29,19 +29,26 @@ export default function FilterBlock({ applyFilters }: FilterBlockProps) {
   }), [minPos, maxPos]);
 
   const handleCategoryChange = (category: string) => {
-    setSelectedCategories((prev) =>
-      prev.includes(category)
-        ? prev.filter((c) => c !== category)
-        : [...prev, category]
-    );
+    setSelectedCategories((prevSelectedCategories) => {
+      if (prevSelectedCategories.includes(category)) {
+        const filteredCategories = prevSelectedCategories.filter((c) => {
+          return c !== category &&
+             !categories.find(cat => cat.label === category)?.options?.includes(c);
+        });
+
+        return filteredCategories;
+      }
+
+      return [...prevSelectedCategories, category];
+    });
   };
 
-  const handleMinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleMinChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = Number(e.target.value);
     if (value <= maxPrice) setMinPrice(value);
   };
 
-  const handleMaxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleMaxChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = Number(e.target.value);
     if (value >= minPrice) setMaxPrice(value);
   };
@@ -56,7 +63,7 @@ export default function FilterBlock({ applyFilters }: FilterBlockProps) {
         <Icon path={WheatYellow} width={47} height={18} />
         Категории
         <Icon path={WheatYellow} width={47} height={18} />
-        </h3>
+      </h3>
       <div className={styles.categories}>
         {categories.map((cat) => (
           <div key={cat.label} className={styles.category}>
@@ -72,7 +79,7 @@ export default function FilterBlock({ applyFilters }: FilterBlockProps) {
               )}
               <h4>{cat.label}</h4>
             </label>
-            {cat.options && (
+            {cat.options && selectedCategories.includes(cat.label) && (
               <div className={styles.subcategories}>
                 {cat.options.map((option) => (
                   <label key={option}>
